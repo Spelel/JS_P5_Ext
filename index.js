05ce7050946912bfbcb56d98f9256a2e4a88c278
Arek <arek-karczo.wp.pl@wp.pl>
2024-09-18 20:29:37 +0200

    For now, done

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 const ledasdFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
@@ -23,6 +24,20 @@ if (ledasdFromLocalStorage) {
     myLeads = ledasdFromLocalStorage
     render(myLeads)
 }
+
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    console.log(tabs[0].url)
+})
 
 function render(leads) {
     let listItems = ""
