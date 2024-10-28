9baacbddc9d94853fce19adc71385702aead8c2a
Arek <arek-karczo.wp.pl@wp.pl>
2024-10-28 19:49:40 +0100

    Change from using local storage to RealTime Database

--- index.js
+++ index.js
@@ -1,49 +1,28 @@
-// function btnClic() {
-//     console.log("Button Click")
-// }
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js"
+import { getDatabase,
+        ref,
+        push,
+        onValue,
+        remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js"
 
-//------------------------------------------------------------------
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-f7047-default-rtdb.europe-west1.firebasedatabase.app/"
+}
 
-// let boxInp = document.getElementById("box")
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
 
-// boxInp.addEventListener("click", function() {
-//     console.log("I want to open the box!")
-// })
 
-//------------------------------------------------------------------
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
 
-const ledasdFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-
-if (ledasdFromLocalStorage) {
-    myLeads = ledasdFromLocalStorage
-    render(myLeads)
-}
-
-
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
-    console.log(tabs[0].url)
-})
 
 function render(leads) {
     let listItems = ""
-    for ( i= 0 ; i < leads.length; i++) {
-        // listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>"
-    
+    for (let i = 0; i < leads.length; i++) {    
         listItems += `
         <li>
             <a target='_blank' href='${leads[i]}'>
@@ -54,23 +33,29 @@ function render(leads) {
     
         }
     ulEl.innerHTML = listItems 
-    }
+}
+
+
+onValue(referenceInDB, function(snapshot) {
+    const snapshotDoesExist = snapshot.exists()
+    if (snapshotDoesExist) {
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+        }
+})
+
+
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
 })
 
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    push(referenceInDB, inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-    
-
-    console.log( localStorage.getItem("myLeads"))
 })
 
 
